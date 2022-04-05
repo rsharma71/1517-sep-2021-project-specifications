ea1ebc84581a8952a8c3bf55bd72347818920c88
rsharma71 <97316190+rsharma71@users.noreply.github.com>
2022-04-04 22:28:35 -0600

    Exercise 6

---
+++ StarTEDWebApp/StarTEDLibruary/StartUpExtention.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StarTEDLibruary.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarTEDLibruary
+{
+    public static class StartUpExtention
+    {
+        public static void StarTEDStartUpDependencies(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            services.AddDbContext<StarTEDContext>(options);
+        }
+    }
+}
