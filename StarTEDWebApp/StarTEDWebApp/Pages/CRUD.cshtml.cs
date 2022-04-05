35427b4f5a435ced080da165d0de5f1be7f7e743
rsharma71 <97316190+rsharma71@users.noreply.github.com>
2022-04-04 22:53:55 -0600

    exercise 6

---
+++ StarTEDWebApp/StarTEDWebApp/Pages/CRUD.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StarTEDWebApp.Pages
+{
+    public class CRUDModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ StarTEDWebApp/StarTEDWebApp/Pages/Queries.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StarTEDWebApp.Pages
+{
+    public class QueriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
